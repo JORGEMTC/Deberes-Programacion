76fff3909c6bf751341e44635bf81b68e5371747
JORGEMTC <105060509+JORGEMTC@users.noreply.github.com>
2022-05-11 08:21:27 -0500

    Add files via upload

---
+++ Practica 4 Eje 5 com.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 21:46:09 2022
+
+@author: Mateo
+"""
+a=500
+b=300
+c=75
+venta=int(input("Ingrese la cantidad de alambre que nesecita:"))
+venta<=a+b+c 
+
+print(f"La tienda le vendera {venta} metros de alambre con un sobrante de metros")
